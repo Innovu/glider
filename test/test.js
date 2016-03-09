97d73fa1863fce36c397c7b8232d5d35fcfc88f6
Tony Lukasavage <alukasavage@innovu.com>
2016-03-09 15:31:38 -0500

    more transaction and query sequence tests

--- test/test.js
+++ test/test.js
@@ -17,20 +17,12 @@ var PORT = process.env.TRAVIS ? '5432' : '55432',
 
 before(function(done) {
 	var query =
-		'DROP TABLE IF EXISTS ' + TABLE + '; ' +
-		'CREATE TABLE ' + TABLE + '(id SERIAL, name VARCHAR(10), age INTEGER);' +
-		'INSERT INTO ' + TABLE + ' (name, age) VALUES ' +
+		'drop table if exists ' + TABLE + '; ' +
+		'create table ' + TABLE + '(id serial, name varchar(10), age integer);' +
+		'insert into ' + TABLE + ' (name, age) values ' +
 		DATA.map(function(d) { return '(\'' + d.name + '\',' + d.age + ')'; }).join(',') + ';';
 
-	// do this with pg, not glider
-	pg.connect(CONSTRING, function(err, client, release) {
-		if (err) { return done(err); }
-		client.query(query, function(err) {
-			if (err) { return done(err); }
-			release();
-			return done();
-		});
-	});
+	pgQuery(query, done);
 });
 
 describe('index', function() {
@@ -49,6 +41,12 @@ describe('index', function() {
 });
 
 describe('[Database]', function() {
+
+	var TABLE_FOO = 'foo';
+
+	after(function(done) {
+		pgQuery('drop table ' + TABLE_FOO, done);
+	});
 
 	it('throws when constructor is invoked without connection string', function() {
 		(function() { new glider.Database(); }).should.throw(/connection string required/);
@@ -101,6 +99,50 @@ describe('[Database]', function() {
 		);
 	});
 
+	it('resolves with test data', function() {
+		return DB.query('select * from ' + TABLE).then(function(result) {
+			result.rowCount.should.equal(DATA.length);
+			DATA.forEach(function(data, index) {
+				result.rows[index].should.have.properties(data);
+			});
+		});
+	});
+
+	it('resolves on create/insert/update/select/delete', function() {
+		return DB.query('create table ' + TABLE_FOO + ' (id serial, bar varchar(10))')
+			.then(function(result) {
+				result.command.should.equal('CREATE');
+				return DB.query('insert into ' + TABLE_FOO + ' (bar) values ($1), ($2), ($3)', ['sanders', 'trump', 'cruz']);
+			})
+			.then(function(result) {
+				result.command.should.equal('INSERT');
+				result.rowCount.should.equal(3);
+				return DB.query('update ' + TABLE_FOO + ' set bar = $1 where bar = $2', ['clinton', 'sanders']);
+			})
+			.then(function(result) {
+				result.command.should.equal('UPDATE');
+				result.rowCount.should.equal(1);
+				return DB.query('select * from ' + TABLE_FOO);
+			})
+			.then(function(result) {
+				result.command.should.equal('SELECT');
+				result.rowCount.should.equal(3);
+				result.fields.should.have.length(2);
+				result.fields.forEach(function(field) { ['id', 'bar'].should.containEql(field.name); });
+				return DB.query('delete from ' + TABLE_FOO + ' where bar != $1', ['trump']);
+			})
+			.then(function(result) {
+				result.command.should.equal('DELETE');
+				result.rowCount.should.equal(2);
+				return DB.query('select * from ' + TABLE_FOO);
+			})
+			.then(function(result) {
+				result.command.should.equal('SELECT');
+				result.rowCount.should.equal(1);
+				result.rows[0].bar.should.equal('trump');
+			});
+	});
+
 });
 
 describe('[Transaction]', function() {
@@ -109,7 +151,7 @@ describe('[Transaction]', function() {
 		(function() { new glider.Transaction(); }).should.throw(/db required/);
 	});
 
-	it('resolves a series of SELECT queries', function() {
+	it('resolves a series of select queries', function() {
 		return DB
 			.begin()
 			.query('select 1::int as number')
@@ -120,7 +162,7 @@ describe('[Transaction]', function() {
 				results[0].rows[0].number.should.equal(1);
 				results[1].rows[0].number.should.equal(2);
 				results[2].number.should.equal(3);
-			}) ;
+			});
 	});
 
 	it('rejects on missing query string', function() {
@@ -149,7 +191,64 @@ describe('[Transaction]', function() {
 			);
 	});
 
+	it('resolves with test data', function() {
+		return DB
+			.begin()
+			.query('select * from ' + TABLE)
+			.commit()
+			.then(function(results) {
+				results.should.have.length(1);
+				results[0].rowCount.should.equal(DATA.length);
+				DATA.forEach(function(data, index) {
+					results[0].rows[index].should.have.properties(data);
+				});
+			});
+	});
+
+	it('resolves on insert and select', function() {
+		return DB
+			.begin()
+			.query('insert into ' + TABLE + ' (name, age) values ($1, $2)', ['rubio', 44])
+			.queryOne('select * from ' + TABLE + ' where id = lastval()')
+			.commit()
+			.then(function(results) {
+				results.should.have.length(2);
+				results[0].rowCount.should.equal(1);
+				results[1].name.should.equal('rubio');
+				results[1].age.should.equal(44);
+			});
+	});
+
+	it('rejects and rollbacks on error', function() {
+		return DB
+			.begin()
+			.query('insert into ' + TABLE + ' (name, age) values ($1, $2)', ['insert1', 123])
+			.query('wtf;')
+			.query('insert into ' + TABLE + ' (name, age) values ($1, $2)', ['insert2', 456])
+			.commit()
+			.then(shouldNotHappen, function(err) {
+				err.message.should.match(/syntax error.+wtf/);
+				return DB.query('select * from ' + TABLE + ' where name in ($1, $2)', ['insert1', 'insert2'])
+			})
+			.then(function(result) {
+				result.rowCount.should.equal(0);
+			});
+	});
+
 });
 
 // helpers
 function shouldNotHappen() { should.not.exist('this should not happen'); }
+
+function pgQuery(query, values, callback) {
+	callback = arguments[arguments.length - 1];
+	values = values || [];
+	pg.connect(CONSTRING, function(err, client, done) {
+		if (err) { return callback(err); }
+		client.query(query, values, function(err) {
+			if (err) { return callback(err); }
+			done();
+			return callback();
+		});
+	});
+}
